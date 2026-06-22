e36cfb18e99c1a1472c07f3998166f0bda0cf73d
kevinklein <klein.k69@web.de>
2026-06-22 15:14:29 +0200

    made ios compatible

--- src/Business.API/Program.cs
+++ src/Business.API/Program.cs
@@ -29,7 +29,10 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("Frontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins(
+                  "http://localhost:3000",      // Next.js dev server (web)
+                  "capacitor://localhost",      // Capacitor iOS webview
+                  "http://localhost")           // Capacitor Android webview
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
